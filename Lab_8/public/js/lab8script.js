129c24b61b2f4fe547e38966e79a6a5711237f7c
DPoludnev <70778747+DPoludnev@users.noreply.github.com>
2020-11-20 19:32:37 -0500

    geee
    der

--- Lab_8/public/js/lab8script.js
+++ Lab_8/public/js/lab8script.js
@@ -1,4 +1,3 @@
-
 let url = "https://www.reddit.com/r/popular.json";
 let chart = "";
 console.log(url);
@@ -7,25 +6,38 @@ console.log(url);
 let settings = { method: "Get" };
 let chartValues = [];
 
+function getRandomInt(min, max) { //getRandomInt(0, listSize)
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min) + min); //The maximum is exclusive and the minimum is inclusive
+  }
+
 async function getData() {
     await fetch(url, settings)
         .then(res => res.json())
         .then((json) => {
             let listSize = json.data.children.length;
+            console.log(listSize)
+
             // Loop to pick 5 random entries
             for (x = 0; x < 5; x++) {
-                let redditList = document.querySelector("redditList");
-                let rdm = math.floor((Math.random() * listSize));
-                let title = json.data.children[rdm].data.title;
-                let subreddit = json.data.children[rdm].data.subreddit;
-                let authot = json.data.children[rdm].data.author;
-                let ups = json.data.children[rdm].data.ups;
+                let random = getRandomInt(0, listSize);
+                let post = json.data.children[random].data;
 
-                let message = "<b>Subreddit </b>:" + subreddit + 
-                "<b> Author</b>:" + author + 
-                "<b>Title</b>:" + title + 
-                "<b>Upvotes <b/>:" + ups;
 
+                console.log(post)
+                let subreddit = post.subreddit;
+                let author = post.author;
+                let title = post.title;
+                let ups = post.ups;
+
+                let message = "<b>Subreddit </b>: " + subreddit + 
+                " <b>Author</b>:" + author + 
+                " <b>Title</b>:" + title + 
+                " <b>Up votes</b>: " + ups;
+
+                let select = document.getElementById("redditList");
+                select.innerHTML += "<li>" + message + "</li>";
                 /*
                     Get a random number within the size of the list
                     Get subreddit, author, title, and ups from record
@@ -35,18 +47,21 @@ async function getData() {
                     Add a data entry to chartValues with author as the label and ups as the y component
                 */
     
-                redditList.innerHTML += "<li>" + message + "</li>";
+
                 /*.......*/
                 
-                let addToChart = {'label':author,y:ups}; // Gave this. This needs to be added to the 'chartValues'
-                chartValues.push(addToChart);/*.......*/
+                let addToChart = {'label':author,y:ups}; 
+                // Gave this. This needs to be added to the 'chartValues'
+                /*.......*/
+                chartValues.push(addToChart);
             }
         })
         .then(values => console.log(chartValues));
-        chart.render(); // Do you need to remove the comments from here in order to get it to work?
+        chart.render(); 
+        // Do you need to remove the comments from here in order to get it to work?
 };
 
-window.onload = async function makeChart() {
+async function makeChart() {
     getData();
     chart = new CanvasJS.Chart("chartContainer", {
         title: {
@@ -65,4 +80,4 @@ window.onload = async function makeChart() {
     chart.render();
 }
 
-//window.onload = makeChart();
+window.onload = makeChart;
--- Lab_8/server.js
+++ Lab_8/server.js
@@ -1,9 +1,11 @@
+
 const express = require('express')
 const app = express()
 
 // Code here for how to set 'public' as the static folder for express
-app.use(experss.static("public"))
+app.use(express.static('public'))
 // Code here to have the app listen on port 8080....please provide a console.log message
-app.listen(port = 8080, () => {
-    console.log("Node at 8080");
+
+app.listen(port = 8080, function(){
+    console.log("port  8080");
 })
